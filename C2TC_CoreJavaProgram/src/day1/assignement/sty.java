3f2b44f3127b4cc79a1d2b391b543082ba799c59
CodebySherif <sherif1029384756@gmail.com>
2025-07-22 13:38:46 +0530

    Assignment 1

---
+++ C2TC_CoreJavaProgram/src/day1/assignement/sty.java
@@ -0,0 +1,14 @@
+package day1.assignement;
+
+public class sty {
+	 public static void main(String[] args) {
+	        Student s1 = new Student(101, "Arun", "IT");
+	        Student s2 = new Student(102, "Divya", "CSE");
+	        Student s3 = new Student(103, "Kiran", "ECE");
+
+	        System.out.println("Student Details:");
+	        s1.display();
+	        s2.display();
+	        s3.display();
+	    }
+}
