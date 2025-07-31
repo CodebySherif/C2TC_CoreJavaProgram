79068ae067ddf8350cf2e5b6a0a269cda8590319
CodebySherif <sherif1029384756@gmail.com>
2025-07-31 12:19:35 +0530

    pushed

---
+++ C2TC_CoreJavaProgram/src/day11/finallyblock/FinallyBlockdemo.java
@@ -0,0 +1,19 @@
+package day11.finallyblock;
+
+public class FinallyBlockdemo {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			System.out.print("Enter the number : ");
+			int number = scanner.nextInt();
+			System.out.println(number % 2 == 0 ? number + " is Even" : number + " is Odd");
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input...");
+
+		} finally {
+			System.out.println("In Finally....");
+			scanner.close();
+		}
+
+	}
+}
